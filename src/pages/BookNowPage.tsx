51bac9936fce0b3928c9abf2d6a0f93d5dc4ed4a
Agorye Jessica <agoryejessica1@gmail.com>
2026-01-15 15:59:28 +0100

    added use params to dipaly event planner using id on booking page

--- src/pages/BookNowPage.tsx
+++ src/pages/BookNowPage.tsx
@@ -1,5 +1,13 @@
+import { Eventplanners } from "../data/eventPlannersData";
+import { useParams } from "react-router-dom";
 
 export const BookNowPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const planner = Eventplanners.find((planner) => planner.id === Number(id));
+
+  if (!planner) {
+    return <p className="text-center mt-20">Planner not found</p>;
+  }
   return (
     <div>
       <div className="max-w-6xl mx-auto px-6">
@@ -9,7 +17,10 @@ export const BookNowPage = () => {
         <p className="text-center text-gray-600 mb-8">
           Complete your booking with the event planner.
         </p>
+        <div>
+          <p>{planner.name}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
